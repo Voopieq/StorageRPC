1ee06031e6465f9036b9f301404e2ac006f1ebbc
Voopieq <domantas.kelmanas2003@gmail.com>
2025-09-14 12:57:06 +0300

    Recreated project files

--- StorageContract/obj/Debug/net8.0/StorageContract.AssemblyInfo.cs
+++ StorageContract/obj/Debug/net8.0/StorageContract.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StorageContract")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d27fb5b1473562f2c4001166f9f5f6e13f42097")]
 [assembly: System.Reflection.AssemblyProductAttribute("StorageContract")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StorageContract")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
